b597bb6d697b8d89b88b693ade152bfe635df089
alejo1630 <66985852+alejo1630@users.noreply.github.com>
2024-05-14 17:47:44 -0500

    1

--- 1. Collinearity.py
+++ 1. Collinearity.py
@@ -19,7 +19,21 @@ def collinearity(x1, y1, x2, y2):
 
         return False
 
-print(collinearity(1,1,1,1))
+
+## Test
+# (1,1,1,1) ➞ true
+# (1,2,2,4) ➞ true
+# (1,1,6,1) ➞ false
+# (1,2,-1,-2) ➞ true
+# (1,2,1,-2) ➞ false
+# (4,0,11,0) ➞ true
+# (0,1,6,0) ➞ false
+# (4,4,0,4) ➞ false
+# (0,0,0,0) ➞ true
+# (0,0,1,0) ➞ true
+# (5,7,0,0) ➞ true
+
+print(collinearity(1,1,1,1)) 
 print(collinearity(1,2,2,4))
 print(collinearity(1,1,6,1))
 print(collinearity(1,2,-1,-2))
